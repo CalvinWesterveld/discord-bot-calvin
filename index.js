cdb916347fb32465436ca3bedc0ff38f3d89451d
CalvinWesterveld <46518111+CalvinWesterveld@users.noreply.github.com>
2019-07-16 00:12:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setActivity(`ShaddowWarrior`);
+  client.user.setPresence({ game: { name: 'ShadowWarriors Clan', type: 0 } });
 });
 
 client.on('guildMemberAdd', member => {
@@ -37,13 +37,13 @@ client.on('guildMemberRemove', member => {
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`ShaddowWarrior`);
+  client.user.setPresence({ game: { name: 'ShadowWarriors Clan', type: 0 } });
 });
 
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`ShaddowWarrior`);
+  client.user.setPresence({ game: { name: 'ShadowWarriors Clan', type: 0 } });
 });
 
 
